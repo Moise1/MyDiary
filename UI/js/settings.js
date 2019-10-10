233a2ad9f476058237b4ea808ece3fd4546b5bd2
Moise1 <moiseri01@gmail.com>
2019-10-10 21:48:16 +0200

    [Start #169075350] initial commit

---
+++ UI/js/settings.js
@@ -0,0 +1,23 @@
+const openNav = ()=>{
+    const mediaQueries = [
+        window.matchMedia("(min-width: 1224px)"),
+        window.matchMedia("(min-width: 700px)"),
+        window.matchMedia("(max-width: 320px)")
+    ]
+    
+    if(mediaQueries[0].matches){
+       return document.getElementById("myNav").style.width = "25%";
+
+    }
+    else if(mediaQueries[1].matches){
+        return document.getElementById("myNav").style.width = "35%";
+
+    }else if(mediaQueries[2]){
+        return document.getElementById("myNav").style.width = "100%";
+    }
+} 
+
+
+const closeNav = ()=>{
+    document.getElementById("myNav").style.width = "0%";
+}
