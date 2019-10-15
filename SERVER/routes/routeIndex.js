0603422bcaa97d668f1aac9164e5915d7eebd37b
Moise1 <moiseri01@gmail.com>
2019-10-15 21:58:08 +0200

    [Finishes #169163190] user can delete an entry

--- SERVER/routes/routeIndex.js
+++ SERVER/routes/routeIndex.js
@@ -15,10 +15,12 @@ router.post("/api/v1/auth/signin", User.SignIn);
 
 
 // Entry router 
+
 router.post("/api/v1/entries", Entry.addEntry); 
 router.get("/api/v1/entries", Entry.allEntries);
 router.get("/api/v1/entries/:entry_id", Entry.singleEntry);
 router.patch("/api/v1/entries/:entry_id", Entry.updateEntry);
+router.delete("/api/v1/entries/:entry_id", Entry.deleteEntry)
 
 
 export default router;
