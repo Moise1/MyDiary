d46783b7a47bb3f81fa83c0b50858819ef3181e1
Moise1 <rwibutso.mo@gmail.com>
2019-11-04 10:45:02 +0200

    Revert "Ch-dbsetup-169531338"

--- SERVER/config/dev.js
+++ SERVER/config/dev.js
@@ -1,10 +1,3 @@
-import dotenv from "dotenv"; 
-
-dotenv.config(); 
-
 export default {
-    DATABASE_URL: process.env.DB_PATH,
-    SECRET_OR_PUBLIC_KEY: process.env.SECRET_OR_PUBLIC_KEY, 
-    DB_PASSWORD: process.env.DB_PASSWORD
-
+    SECRET_OR_PUBLIC_KEY:"mydiary"
 }
